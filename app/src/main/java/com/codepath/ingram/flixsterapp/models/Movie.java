c004e3f90c5c05ae955a5d825ce0574c15a997a3
Ing-Ram <chad117gram@gmail.com>
2021-02-06 15:12:23 -0800

    added walkthrough.gif

--- app/src/main/java/com/codepath/ingram/flixsterapp/models/Movie.java
+++ app/src/main/java/com/codepath/ingram/flixsterapp/models/Movie.java
@@ -3,19 +3,28 @@ package com.codepath.ingram.flixsterapp.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Parcel
 public class Movie {
+    int movieId;
     String posterPath;
     String title;
     String overveiw;
+    double rating;
+
+    public Movie(){}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overveiw = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
         List<Movie> movies = new ArrayList<>();
@@ -36,5 +45,11 @@ public class Movie {
 
     public String getOverveiw() {
         return overveiw;
+    }
+
+    public double getRating() { return rating; }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
